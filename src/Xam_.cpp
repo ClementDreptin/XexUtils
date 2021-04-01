041491121fe0b9e9861026bb17b40f19db849fed
Clement <cdreptin@gmail.com>
2021-04-01 16:33:39 +0200

    Added a ShowKeyboard function

--- src/Xam_.cpp
+++ src/Xam_.cpp
@@ -25,5 +25,29 @@ namespace Xam
 	{
 		XNotifyQueueUI(type, 0xFF, XNOTIFY_SYSTEM, (PWCHAR)(Formatter::ToWide(text).c_str()), 0);
 	}
+
+	std::string ShowKeyboard(const std::string& title, const std::string& description, const std::string& defaultValue, size_t maxLength, DWORD keyboardType)
+	{
+		// maxLength is the amount of characters the keyboard will allow, realMaxLength needs to include the \0 to terminate the string
+		size_t realMaxLength = maxLength + 1;
+		XOVERLAPPED overlapped;
+		wchar_t* wideBuffer = new wchar_t[realMaxLength];
+		char* buffer = new char[realMaxLength];
+
+		ZeroMemory(&overlapped, sizeof(overlapped));
+		XShowKeyboardUI(0, keyboardType, Formatter::ToWide(defaultValue).c_str(), Formatter::ToWide(title).c_str(), Formatter::ToWide(description).c_str(), wideBuffer, realMaxLength, &overlapped);
+
+		while (!XHasOverlappedIoCompleted(&overlapped))
+			Sleep(100);
+
+		wcstombs(buffer, wideBuffer, realMaxLength);
+
+		std::string result = buffer;
+
+		delete[] wideBuffer;
+		delete[] buffer;
+
+		return result;
+	}
 }
 }
