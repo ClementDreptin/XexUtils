9cca72d01652ffa16f47f230a5e9d1581331a108
Clément Dreptin <cdreptin@gmail.com>
2024-12-13 16:07:52 +0100

    fix(math): RoundToNextMultiple only working for powers of 2

--- src/Math_.h
+++ src/Math_.h
@@ -16,7 +16,12 @@ vec3 ProjectForward(const vec3 &origin, float viewY, float distance);
 template<uint32_t N>
 uint32_t RoundToNextMultipleOf(uint32_t base)
 {
-    return (base + N) & ~(N - 1); // Believe it or not, it works
+    if (N == 0)
+        return base;
+
+    uint32_t remainder = base % N;
+
+    return base + (N - remainder);
 }
 
 }
