adfd5f35d827afb32d55c9fa2d49fd4fde2adedd
Clément Dreptin <cdreptin@gmail.com>
2024-12-13 16:39:14 +0100

    fix(math): use compile time check instead of runtime if to make sure N isn't 0 in RoundToNextMultipleOf

--- src/Math_.h
+++ src/Math_.h
@@ -16,8 +16,7 @@ vec3 ProjectForward(const vec3 &origin, float viewY, float distance);
 template<uint32_t N>
 uint32_t RoundToNextMultipleOf(uint32_t base)
 {
-    if (N == 0)
-        return base;
+    static_assert(N != 0, "Multiplier N cannot be zero.");
 
     uint32_t remainder = base % N;
 
