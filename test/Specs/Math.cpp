56576911ace848371b9ae581f8c323b94c0bc776
Clément Dreptin <cdreptin@gmail.com>
2026-08-25 18:02:37 +0200

    test(math): used the function signatures as describe labels

--- test/Specs/Math.cpp
+++ test/Specs/Math.cpp
@@ -7,13 +7,13 @@ using namespace TestRunner;
 
 void Math()
 {
-    Describe("Math::Radians");
+    Describe("Math::Radians(float)");
 
     It("converts degrees to radians", []() {
         TEST_BETWEEN(Math::Radians(45.0f), 0.78f, 0.79f);
     });
 
-    Describe("Math::ProjectForward");
+    Describe("Math::ProjectForward(const Math::vec3 &, float, float)");
 
     It("projects a vector based on an orientation", []() {
         Math::vec3 v1(1.0f, 2.0f, 3.0f);
@@ -24,9 +24,9 @@ void Math()
         TEST_EQ(v2.z, 3.0f);
     });
 
-    Describe("Math::RoundToNextMultiple");
+    Describe("Math::RoundToNextMultiple<N>(uint32_t)");
 
-    It("rounds to the next multiple", []() {
+    It("rounds to the next multiple of N", []() {
         TEST_EQ(Math::RoundToNextMultipleOf<3>(4), 6);
         TEST_EQ(Math::RoundToNextMultipleOf<5>(2), 5);
         TEST_EQ(Math::RoundToNextMultipleOf<4>(4), 8);
