d74833cdd65a20928a848b4e321658ce5024a7cf
Clément Dreptin <cdreptin@gmail.com>
2026-08-25 18:13:11 +0200

    test(vec4): used the function signatures as describe labels

--- test/Specs/Vec4.cpp
+++ test/Specs/Vec4.cpp
@@ -7,62 +7,244 @@ using namespace TestRunner;
 
 void Vec4()
 {
-    Describe("Math::vec4");
+    Describe("Math::vec4()");
 
-    Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
-    Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+    It("creates an empty vec4", []() {
+        auto v1 = Math::vec4();
 
-    It("calculates the magnitude", [=]() {
-        TEST_BETWEEN(v2.magnitude(), 13.19f, 13.20f);
+        TEST_EQ(v1.x, 0.0f);
+        TEST_EQ(v1.y, 0.0f);
+        TEST_EQ(v1.z, 0.0f);
+        TEST_EQ(v1.w, 0.0f);
     });
 
-    It("normalizes itself", [=]() {
-        Math::vec4 result = v2.normalize();
+    Describe("Math::vec4(float, float, float, float)");
+
+    It("creates a vec4 from x, y, z and w", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+
+        TEST_EQ(v1.x, 1.0f);
+        TEST_EQ(v1.y, 2.0f);
+        TEST_EQ(v1.z, 3.0f);
+        TEST_EQ(v1.w, 4.0f);
+    });
+
+    Describe("Math::vec4::magnitude()");
+
+    It("calculates the magnitude", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        float magnitude = v1.magnitude();
+
+        TEST_BETWEEN(magnitude, 13.19f, 13.20f);
+    });
+
+    Describe("Math::vec4::normalize()");
+
+    It("normalizes the current vec4", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 result = v1.normalize();
 
         TEST_BETWEEN(result.x, 0.37f, 0.38f);
         TEST_BETWEEN(result.y, 0.45f, 0.46f);
+        TEST_BETWEEN(result.z, 0.53f, 0.54f);
+        TEST_BETWEEN(result.w, 0.60f, 0.61f);
     });
 
-    It("knows when it's null", [=]() {
-        Math::vec4 null(0.0f, 0.0f, 0.0f, 0.0f);
+    Describe("Math::vec4::add(const Math::vec4 &)");
 
-        TEST_EQ(null.isNull(), true);
-        TEST_EQ(v2.isNull(), false);
+    It("adds the other vec4 to the current vec4", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+        v1.add(v2);
+
+        TEST_EQ(v1.x, 6.0f);
+        TEST_EQ(v1.y, 8.0f);
+        TEST_EQ(v1.z, 10.0f);
+        TEST_EQ(v1.w, 12.0f);
     });
 
-    It("performs equality", [=]() {
-        TEST_EQ(v1 == v1, true);
+    Describe("Math::vec4::substract(const Math::vec4 &)");
+
+    It("substracts the other vec4 from the current vec4", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 v2(1.0f, 2.0f, 3.0f, 4.0f);
+        v1.substract(v2);
+
+        TEST_EQ(v1.x, 4.0f);
+        TEST_EQ(v1.y, 4.0f);
+        TEST_EQ(v1.z, 4.0f);
+        TEST_EQ(v1.w, 4.0f);
+    });
+
+    Describe("Math::vec4::multiply(const Math::vec4 &)");
+
+    It("multiplies the current vec4 by the other vec4", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+        v1.multiply(v2);
+
+        TEST_EQ(v1.x, 5.0f);
+        TEST_EQ(v1.y, 12.0f);
+        TEST_EQ(v1.z, 21.0f);
+        TEST_EQ(v1.w, 32.0f);
+    });
+
+    Describe("Math::vec4::multiply(const float &)");
+
+    It("multiplies the current vec4 by the multiplier", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        float multiplier = 2.0f;
+        v1.multiply(multiplier);
+
+        TEST_EQ(v1.x, 2.0f);
+        TEST_EQ(v1.y, 4.0f);
+        TEST_EQ(v1.z, 6.0f);
+        TEST_EQ(v1.w, 8.0f);
+    });
+
+    Describe("Math::vec4::divide(const Math::vec4 &)");
+
+    It("divides the current vec4 by the other vec4", []() {
+        Math::vec4 v1(4.0f, 6.0f, 8.0f, 10.0f);
+        Math::vec4 v2(2.0f, 3.0f, 4.0f, 5.0f);
+        v1.divide(v2);
+
+        TEST_EQ(v1.x, 2.0f);
+        TEST_EQ(v1.y, 2.0f);
+        TEST_EQ(v1.z, 2.0f);
+        TEST_EQ(v1.w, 2.0f);
+    });
+
+    Describe("Math::vec4::divide(const float &)");
+
+    It("divides the current vec4 by the divider", []() {
+        Math::vec4 v1(4.0f, 6.0f, 8.0f, 10.0f);
+        float divider = 2.0f;
+        v1.divide(divider);
+
+        TEST_EQ(v1.x, 2.0f);
+        TEST_EQ(v1.y, 3.0f);
+        TEST_EQ(v1.z, 4.0f);
+        TEST_EQ(v1.w, 5.0f);
+    });
+
+    Describe("Math::vec4::isNull()");
+
+    It("returns true when the vec4 is null", []() {
+        Math::vec4 v1(0.0f, 0.0f, 0.0f, 0.0f);
+
+        TEST_EQ(v1.isNull(), true);
+    });
+
+    It("returns false when the vec4 is not null", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+
+        TEST_EQ(v1.isNull(), false);
+    });
+
+    Describe("Math::vec4::operator==(const vec4 &)");
+
+    It("returns true when the vec4s are equal", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(1.0f, 2.0f, 3.0f, 4.0f);
+
+        TEST_EQ(v1 == v2, true);
+    });
+
+    It("returns false when the vec4s are not equal", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+
         TEST_EQ(v1 == v2, false);
     });
 
-    It("performs inequality", [=]() {
-        TEST_EQ(v1 != v1, false);
+    Describe("Math::vec4::operator!=(const vec4 &)");
+
+    It("returns true when the vec4s not are equal", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+
         TEST_EQ(v1 != v2, true);
     });
 
-    It("performs less than", [=]() {
-        TEST_EQ(v1 < v2, true);
-        TEST_EQ(v2 < v1, false);
+    It("returns false when the vec4s are equal", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(1.0f, 2.0f, 3.0f, 4.0f);
+
+        TEST_EQ(v1 != v2, false);
     });
 
-    It("performs less than or equal to", [=]() {
+    Describe("Math::vec4::operator<(const vec4 &)");
+
+    It("returns true when v1 is less than v2", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+
+        TEST_EQ(v1 < v2, true);
+    });
+
+    It("returns false when v1 is greater than v2", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 v2(1.0f, 2.0f, 3.0f, 4.0f);
+
+        TEST_EQ(v1 < v2, false);
+    });
+
+    Describe("Math::vec4::operator<=(const vec4 &)");
+
+    It("returns true when v1 is less than or equal to v2", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+
         TEST_EQ(v1 <= v2, true);
         TEST_EQ(v1 <= v1, true);
-        TEST_EQ(v2 <= v1, false);
     });
 
-    It("performs greater than", [=]() {
+    It("returns false when v1 is greater than v2", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 v2(1.0f, 2.0f, 3.0f, 4.0f);
+
+        TEST_EQ(v1 <= v2, false);
+    });
+
+    Describe("Math::vec4::operator>(const vec4 &)");
+
+    It("returns true when v1 is greater than v2", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 v2(1.0f, 2.0f, 3.0f, 4.0f);
+
+        TEST_EQ(v1 > v2, true);
+    });
+
+    It("returns false when v1 is less than v2", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+
         TEST_EQ(v1 > v2, false);
-        TEST_EQ(v2 > v1, true);
     });
 
-    It("performs greater than or equal to", [=]() {
-        TEST_EQ(v1 >= v2, false);
+    Describe("Math::vec4::operator>=(const vec4 &)");
+
+    It("returns true when v1 is greater than or equal to v2", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 v2(1.0f, 2.0f, 3.0f, 4.0f);
+
+        TEST_EQ(v1 >= v2, true);
         TEST_EQ(v1 >= v1, true);
-        TEST_EQ(v2 >= v1, true);
     });
 
-    It("performs addition", [=]() {
+    It("returns false when v1 is less than v2", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
+
+        TEST_EQ(v1 >= v2, false);
+    });
+
+    Describe("Math::vec4::operator+(const vec4 &)");
+
+    It("adds v1 to v2", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
         Math::vec4 result = v1 + v2;
 
         TEST_EQ(result.x, 6.0f);
@@ -71,16 +253,24 @@ void Vec4()
         TEST_EQ(result.w, 12.0f);
     });
 
-    It("performs substraction", [=]() {
+    Describe("Math::vec4::operator-(const vec4 &)");
+
+    It("substracts v2 from v1", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 v2(1.0f, 2.0f, 3.0f, 4.0f);
         Math::vec4 result = v1 - v2;
 
-        TEST_EQ(result.x, -4.0f);
-        TEST_EQ(result.y, -4.0f);
-        TEST_EQ(result.z, -4.0f);
-        TEST_EQ(result.w, -4.0f);
+        TEST_EQ(result.x, 4.0f);
+        TEST_EQ(result.y, 4.0f);
+        TEST_EQ(result.z, 4.0f);
+        TEST_EQ(result.w, 4.0f);
     });
 
-    It("performs multiplication with another vec4", [=]() {
+    Describe("Math::vec4::operator*(const vec4 &)");
+
+    It("multiplies v1 with v2", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 v2(5.0f, 6.0f, 7.0f, 8.0f);
         Math::vec4 result = v1 * v2;
 
         TEST_EQ(result.x, 5.0f);
@@ -89,7 +279,10 @@ void Vec4()
         TEST_EQ(result.w, 32.0f);
     });
 
-    It("performs multiplication with a float", [=]() {
+    Describe("Math::vec4::operator*(const float &)");
+
+    It("multiplies v1 with a float", []() {
+        Math::vec4 v1(1.0f, 2.0f, 3.0f, 4.0f);
         Math::vec4 result = v1 * 3.0f;
 
         TEST_EQ(result.x, 3.0f);
@@ -98,8 +291,12 @@ void Vec4()
         TEST_EQ(result.w, 12.0f);
     });
 
-    It("performs division with another vec4", [=]() {
-        Math::vec4 result = v2 / v1;
+    Describe("Math::vec4::operator/(const vec4 &)");
+
+    It("divides v1 by v2", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 v2(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::vec4 result = v1 / v2;
 
         TEST_EQ(result.x, 5.0f);
         TEST_EQ(result.y, 3.0f);
@@ -107,8 +304,11 @@ void Vec4()
         TEST_EQ(result.w, 2.0f);
     });
 
-    It("performs division with a float", [=]() {
-        Math::vec4 result = v2 / 2.0f;
+    Describe("Math::vec4::operator/(const float &)");
+
+    It("divides v1 by a float", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 result = v1 / 2.0f;
 
         TEST_EQ(result.x, 2.5f);
         TEST_EQ(result.y, 3.0f);
@@ -116,8 +316,11 @@ void Vec4()
         TEST_EQ(result.w, 4.0f);
     });
 
-    It("performs the opposite", [=]() {
-        Math::vec4 result = -v2;
+    Describe("Math::vec4::operator-()");
+
+    It("returns the opposite", []() {
+        Math::vec4 v1(5.0f, 6.0f, 7.0f, 8.0f);
+        Math::vec4 result = -v1;
 
         TEST_EQ(result.x, -5.0f);
         TEST_EQ(result.y, -6.0f);
