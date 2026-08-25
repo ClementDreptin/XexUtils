203844baffc960c4e3eb01d76290e7a720321606
Clément Dreptin <cdreptin@gmail.com>
2026-08-25 18:12:52 +0200

    test(vec2): used the function signatures as describe labels

--- test/Specs/Vec2.cpp
+++ test/Specs/Vec2.cpp
@@ -7,107 +7,290 @@ using namespace TestRunner;
 
 void Vec2()
 {
-    Describe("Math::vec2");
+    Describe("Math::vec2()");
 
-    Math::vec2 v1(1.0f, 2.0f);
-    Math::vec2 v2(3.0f, 4.0f);
+    It("creates an empty vec2", []() {
+        auto v1 = Math::vec2();
 
-    It("calculates the magnitude", [=]() {
-        float magnitude = v2.magnitude();
+        TEST_EQ(v1.x, 0.0f);
+        TEST_EQ(v1.y, 0.0f);
+    });
+
+    Describe("Math::vec2(float, float)");
+
+    It("creates a vec2 from x and y", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+
+        TEST_EQ(v1.x, 1.0f);
+        TEST_EQ(v1.y, 2.0f);
+    });
+
+    Describe("Math::vec2::magnitude()");
+
+    It("calculates the magnitude", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        float magnitude = v1.magnitude();
 
         TEST_EQ(magnitude, 5);
     });
 
-    It("normalizes itself", [=]() {
-        Math::vec2 result = v2.normalize();
+    Describe("Math::vec2::normalize()");
+
+    It("normalizes the current vec2", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 result = v1.normalize();
 
         TEST_EQ(result.x, 0.6f);
         TEST_EQ(result.y, 0.8f);
     });
 
-    It("knows when it's null", [=]() {
-        Math::vec2 null(0.0f, 0.0f);
+    Describe("Math::vec2::add(const Math::vec2 &)");
 
-        TEST_EQ(null.isNull(), true);
-        TEST_EQ(v2.isNull(), false);
+    It("adds the other vec2 to the current vec2", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
+        v1.add(v2);
+
+        TEST_EQ(v1.x, 4.0f);
+        TEST_EQ(v1.y, 6.0f);
     });
 
-    It("performs equality", [=]() {
-        TEST_EQ(v1 == v1, true);
+    Describe("Math::vec2::substract(const Math::vec2 &)");
+
+    It("substracts the other vec2 from the current vec2", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 v2(1.0f, 2.0f);
+        v1.substract(v2);
+
+        TEST_EQ(v1.x, 2.0f);
+        TEST_EQ(v1.y, 2.0f);
+    });
+
+    Describe("Math::vec2::multiply(const Math::vec2 &)");
+
+    It("multiplies the current vec2 by the other vec2", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
+        v1.multiply(v2);
+
+        TEST_EQ(v1.x, 3.0f);
+        TEST_EQ(v1.y, 8.0f);
+    });
+
+    Describe("Math::vec2::multiply(const float &)");
+
+    It("multiplies the current vec2 by the multiplier", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        float multiplier = 2.0f;
+        v1.multiply(multiplier);
+
+        TEST_EQ(v1.x, 2.0f);
+        TEST_EQ(v1.y, 4.0f);
+    });
+
+    Describe("Math::vec2::divide(const Math::vec2 &)");
+
+    It("divides the current vec2 by the other vec2", []() {
+        Math::vec2 v1(4.0f, 6.0f);
+        Math::vec2 v2(2.0f, 3.0f);
+        v1.divide(v2);
+
+        TEST_EQ(v1.x, 2.0f);
+        TEST_EQ(v1.y, 2.0f);
+    });
+
+    Describe("Math::vec2::divide(const float &)");
+
+    It("divides the current vec2 by the divider", []() {
+        Math::vec2 v1(4.0f, 6.0f);
+        float divider = 2.0f;
+        v1.divide(divider);
+
+        TEST_EQ(v1.x, 2.0f);
+        TEST_EQ(v1.y, 3.0f);
+    });
+
+    Describe("Math::vec2::isNull()");
+
+    It("returns true when the vec2 is null", []() {
+        Math::vec2 v1(0.0f, 0.0f);
+
+        TEST_EQ(v1.isNull(), true);
+    });
+
+    It("returns false when the vec2 is not null", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+
+        TEST_EQ(v1.isNull(), false);
+    });
+
+    Describe("Math::vec2::operator==(const vec2 &)");
+
+    It("returns true when the vec2s are equal", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(1.0f, 2.0f);
+
+        TEST_EQ(v1 == v2, true);
+    });
+
+    It("returns false when the vec2s are not equal", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
+
         TEST_EQ(v1 == v2, false);
     });
 
-    It("performs inequality", [=]() {
-        TEST_EQ(v1 != v1, false);
+    Describe("Math::vec2::operator!=(const vec2 &)");
+
+    It("returns true when the vec2s not are equal", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
+
         TEST_EQ(v1 != v2, true);
     });
 
-    It("performs less than", [=]() {
-        TEST_EQ(v1 < v2, true);
-        TEST_EQ(v2 < v1, false);
+    It("returns false when the vec2s are equal", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(1.0f, 2.0f);
+
+        TEST_EQ(v1 != v2, false);
     });
 
-    It("performs less than or equal to", [=]() {
+    Describe("Math::vec2::operator<(const vec2 &)");
+
+    It("returns true when v1 is less than v2", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
+
+        TEST_EQ(v1 < v2, true);
+    });
+
+    It("returns false when v1 is greater than v2", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 v2(1.0f, 2.0f);
+
+        TEST_EQ(v1 < v2, false);
+    });
+
+    Describe("Math::vec2::operator<=(const vec2 &)");
+
+    It("returns true when v1 is less than or equal to v2", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
+
         TEST_EQ(v1 <= v2, true);
         TEST_EQ(v1 <= v1, true);
-        TEST_EQ(v2 <= v1, false);
     });
 
-    It("performs greater than", [=]() {
+    It("returns false when v1 is greater than v2", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 v2(1.0f, 2.0f);
+
+        TEST_EQ(v1 <= v2, false);
+    });
+
+    Describe("Math::vec2::operator>(const vec2 &)");
+
+    It("returns true when v1 is greater than v2", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 v2(1.0f, 2.0f);
+
+        TEST_EQ(v1 > v2, true);
+    });
+
+    It("returns false when v1 is less than v2", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
+
         TEST_EQ(v1 > v2, false);
-        TEST_EQ(v2 > v1, true);
     });
 
-    It("performs greater than or equal to", [=]() {
-        TEST_EQ(v1 >= v2, false);
+    Describe("Math::vec2::operator>=(const vec2 &)");
+
+    It("returns true when v1 is greater than or equal to v2", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 v2(1.0f, 2.0f);
+
+        TEST_EQ(v1 >= v2, true);
         TEST_EQ(v1 >= v1, true);
-        TEST_EQ(v2 >= v1, true);
     });
 
-    It("performs addition", [=]() {
+    It("returns false when v1 is less than v2", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
+
+        TEST_EQ(v1 >= v2, false);
+    });
+
+    Describe("Math::vec2::operator+(const vec2 &)");
+
+    It("adds v1 to v2", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
         Math::vec2 result = v1 + v2;
 
         TEST_EQ(result.x, 4.0f);
         TEST_EQ(result.y, 6.0f);
     });
 
-    It("performs substraction", [=]() {
+    Describe("Math::vec2::operator-(const vec2 &)");
+
+    It("substracts v2 from v1", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 v2(1.0f, 2.0f);
         Math::vec2 result = v1 - v2;
 
-        TEST_EQ(result.x, -2.0f);
-        TEST_EQ(result.y, -2.0f);
+        TEST_EQ(result.x, 2.0f);
+        TEST_EQ(result.y, 2.0f);
     });
 
-    It("performs multiplication with another vec2", [=]() {
+    Describe("Math::vec2::operator*(const vec2 &)");
+
+    It("multiplies v1 with v2", []() {
+        Math::vec2 v1(1.0f, 2.0f);
+        Math::vec2 v2(3.0f, 4.0f);
         Math::vec2 result = v1 * v2;
 
         TEST_EQ(result.x, 3.0f);
         TEST_EQ(result.y, 8.0f);
     });
 
-    It("performs multiplication with a float", [=]() {
+    Describe("Math::vec2::operator*(const float &)");
+
+    It("multiplies v1 with a float", []() {
+        Math::vec2 v1(1.0f, 2.0f);
         Math::vec2 result = v1 * 3.0f;
 
         TEST_EQ(result.x, 3.0f);
         TEST_EQ(result.y, 6.0f);
     });
 
-    It("performs division with another vec2", [=]() {
-        Math::vec2 result = v2 / v1;
+    Describe("Math::vec2::operator/(const vec2 &)");
+
+    It("divides v1 by v2", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 v2(1.0f, 2.0f);
+        Math::vec2 result = v1 / v2;
 
         TEST_EQ(result.x, 3.0f);
         TEST_EQ(result.y, 2.0f);
     });
 
-    It("performs division with a float", [=]() {
-        Math::vec2 result = v2 / 2.0f;
+    Describe("Math::vec2::operator/(const float &)");
+
+    It("divides v1 by a float", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 result = v1 / 2.0f;
 
         TEST_EQ(result.x, 1.5f);
         TEST_EQ(result.y, 2.0f);
     });
 
-    It("performs the opposite", [=]() {
-        Math::vec2 result = -v2;
+    Describe("Math::vec2::operator-()");
+
+    It("returns the opposite", []() {
+        Math::vec2 v1(3.0f, 4.0f);
+        Math::vec2 result = -v1;
 
         TEST_EQ(result.x, -3.0f);
         TEST_EQ(result.y, -4.0f);
